c3b29b39da718192aceb16e6ffc6571e3d87316c
Burak Turk <burakturk.b@gmail.com>
2021-03-05 22:47:07 -0800

    prepare semester and course filter options data

--- client/src/components/ReviewCardListConnected/components/Toolbar/Toolbar.container.tsx
+++ client/src/components/ReviewCardListConnected/components/Toolbar/Toolbar.container.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { ReviewSortKey as SortKey } from 'src/core/types';
+import {
+  Option,
+  ReviewSortKey as SortKey,
+  SemesterOption,
+} from 'src/core/types';
 import { useCoursesQuery, useSemestersQuery } from 'src/graphql';
 
 import Toolbar, { Props as ChildProps } from './Toolbar';
@@ -17,16 +21,18 @@ const sortKeyOptions = [
 const ToolbarContainer: React.FC<Props> = (props) => {
   const [courses, semesters] = [useCoursesQuery(), useSemestersQuery()];
 
-  const courseFilterOptions =
+  const courseFilterOptions: Option[] =
     courses.data?.courses?.map((course) => ({
       value: course.id,
       label: `${course.id} ${course.name}`,
     })) || [];
 
-  const semesterFilterOptions =
+  const semesterFilterOptions: SemesterOption[] =
     semesters.data?.semesters?.map((semester) => ({
       value: semester.id,
-      label: semester.name,
+      label: semester.name.split(' ')[0],
+      year: semester.year,
+      season: semester.season,
     })) || [];
 
   return (
